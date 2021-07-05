b513e7187aeafb946c708ff1dcbb576d7737b36c
mrarthurwhite <mister.arthur.white@gmail.com>
2021-07-04 23:56:52 -0400

    converted the sqllite code into a jar file that can be used later

--- src/com/test/db/App.java
+++ src/com/test/db/App.java
@@ -7,12 +7,12 @@ public class App {
 	public static void main(String[] args) {
 		UserDao dao = new UserDao();
 		// Add new user
-		User user = new User();
+		/*User user = new User();
 		user.setFirstName("Arthur");
 		dao.addUser(user);
 		// Update user
 		user.setFirstName("Arthur White");
-		dao.updateUser(user);
+		dao.updateUser(user);*/
 		// Delete user// dao.deleteUser(1);
 		// Get all users
 		for (User u : dao.getAllUsers()) {
